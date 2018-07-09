c38df81531f37af578196e9b9b2fb26aff8483b1
Nikita Alekseev <n.alekseev2104@gmail.com>
2018-07-09 11:54:22 +0300

    Refactor proposal to be noncopyable
    Signed-off-by: Nikita Alekseev <n.alekseev2104@gmail.com>
    
    # Conflicts:
    #	test/benchmark/CMakeLists.txt
    #	test/benchmark/bm_proto_creation.cpp

--- shared_model/backend/protobuf/proposal.cpp
+++ shared_model/backend/protobuf/proposal.cpp
@@ -4,3 +4,30 @@
  */
 
 #include "backend/protobuf/proposal.hpp"
+
+namespace shared_model {
+  namespace proto {
+    using namespace interface::types;
+
+    Proposal::Proposal(Proposal &&o) noexcept
+        : NonCopyableProto(std::move(o.proto_)) {}
+
+    Proposal &Proposal::operator=(Proposal &&o) noexcept {
+      proto_ = std::move(o.proto_);
+      transactions_.invalidate();
+    }
+
+    TransactionsCollectionType Proposal::transactions() const {
+      return *transactions_;
+    }
+
+    TimestampType Proposal::createdTime() const {
+      return proto_.created_time();
+    }
+
+    HeightType Proposal::height() const {
+      return proto_.height();
+    }
+
+  }  // namespace proto
+}  // namespace shared_model
