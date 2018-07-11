16b44be845fd5f510f66d14267cbe1b754b6b11e
rodionov12 <rodionov12@gmail.com>
2018-07-11 07:58:21 +0300

    Python bindings - fix sed Mac OS
    Signed-off-by: rodionov12 <rodionov12@gmail.com>

--- .jenkinsci/bindings.groovy
+++ .jenkinsci/bindings.groovy
@@ -198,10 +198,10 @@ def doPythonWheels(os, buildType) {
   else {
     sh "sed -i 's/{{ PYPI_VERSION }}/${version}/g' wheels/setup.py;"
   }
-  sh """
-    modules=($(find wheels/iroha -type f -not -name '__init__.py' | sed 's/wheels\/iroha\///g' | grep '\.py$' | sed -e 's/\..*$//')); \
-    for f in wheels/iroha/*.py; do for m in "\${modules[@]}"; do sed -i '' 's/import \$m/from . import \$m/g' \$f; done; done;
-  """
+  // sh """
+  //   modules=($(find wheels/iroha -type f -not -name '__init__.py' | sed 's/wheels\/iroha\///g' | grep '\.py$' | sed -e 's/\..*$//')); \
+  //   for f in wheels/iroha/*.py; do for m in "\${modules[@]}"; do sed -i '' 's/import \$m/from . import \$m/g' \$f; done; done;
+  // """
   if (os == 'linux') {
     sh "${envs} wheel --no-deps wheels/;"
   }
