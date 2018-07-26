a8fce3605b41bcd170eb0836409f892a0c03b440
Akvinikym <anarant12@gmail.com>
2018-07-26 15:13:54 +0300

    Multithreading test is implemented
    Signed-off-by: Akvinikym <anarant12@gmail.com>

--- test/module/irohad/ametsuchi/consensus_cache_test.cpp
+++ test/module/irohad/ametsuchi/consensus_cache_test.cpp
@@ -9,11 +9,10 @@
 #include "module/irohad/ametsuchi/ametsuchi_fixture.hpp"
 #include "module/shared_model/builders/protobuf/test_block_builder.hpp"
 
- class ConsensusCacheBlockTest : public ::testing::Test {
+class ConsensusCacheBlockTest : public ::testing::Test {
  protected:
   void SetUp() override {
-    consensus_cache_block =
-        std::make_shared<ConsensusCacheBlock>();
+    consensus_cache_block = std::make_shared<ConsensusCacheBlock>();
   }
 
   void TearDown() override {
@@ -46,4 +45,37 @@ TEST_F(ConsensusCacheBlockTest, SingleThreadedCache) {
   ASSERT_FALSE(consensus_cache_block->get());
 }
 
-TEST_F(ConsensusCacheBlockTest, MultithreadedCache) {}
+/**
+ * @given up and running consensus cache for blocks
+ * @when first thread inserts value @and another tries to read it @and the first
+ * removes it @and second read operation completes
+ * @then the system could possibly crash, if it was not thread-safe - it would
+ * try to give removed value to the second thread
+ */
+TEST_F(ConsensusCacheBlockTest, MultithreadedCache) {
+  std::mutex mutex;
+  std::condition_variable cv;
+  bool reader_has_started = false;
+
+  auto block = std::make_shared<shared_model::proto::Block>(
+      TestBlockBuilder().height(default_block_height).build());
+  consensus_cache_block->insert(
+      std::make_shared<shared_model::interface::BlockVariant>(
+          std::move(block)));
+  ASSERT_TRUE(consensus_cache_block->get());
+
+  // we are going to wait until reader wakes up before releasing the cache,
+  // otherwise there's no sense in the test
+  std::thread reader{[this, &reader_has_started, &cv]() {
+    reader_has_started = true;
+    cv.notify_all();
+    ASSERT_TRUE(consensus_cache_block->get().get());
+  }};
+  std::unique_lock<std::mutex> lock(mutex);
+  cv.wait(lock, [&reader_has_started]() { return reader_has_started; });
+
+  consensus_cache_block->release();
+  reader.join();
+
+  ASSERT_FALSE(consensus_cache_block->get());
+}
