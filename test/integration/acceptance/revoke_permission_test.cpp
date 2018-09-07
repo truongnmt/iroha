a60485b9fc3fb91f8e01ea2f0468c6f0f3f058a4
kamilsa <kamilsa16@gmail.com>
2018-09-07 09:32:59 +0300

    Fix last test
    Signed-off-by: kamilsa <kamilsa16@gmail.com>

--- test/integration/acceptance/revoke_permission_test.cpp
+++ test/integration/acceptance/revoke_permission_test.cpp
@@ -331,14 +331,11 @@ namespace grantables {
         .skipVerifiedProposal()
         .checkBlock(
             [](auto &block) { ASSERT_EQ(block->transactions().size(), 1); })
-        .sendTx(
+        .sendTxAwait(
             gpf::revokePermission(gpf::kAccount1,
                                   gpf::kAccount1Keypair,
                                   gpf::kAccount2,
-                                  this->grantable_type_.grantable_permission_))
-        .skipProposal()
-        .skipVerifiedProposal()
-        .checkBlock(
+                                  this->grantable_type_.grantable_permission_),
             // permission was successfully revoked
             [](auto &block) { ASSERT_EQ(block->transactions().size(), 1); });
     auto last_check_tx = this->grantable_type_.testTransaction(*this);
@@ -355,6 +352,8 @@ namespace grantables {
         .checkProposal([](auto &proposal) {
           ASSERT_EQ(proposal->transactions().size(), 1);
         })
+        .skipVerifiedProposal()
+        .skipBlock()
         .getTxStatus(last_check_tx.hash(),
                      [](auto &status) {
                        auto message = status.errorMessage();
